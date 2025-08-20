5d84936eee50331247b0f1b67b37b1e7f1380e24
Ashraf Sellak <sellakachraf9@gmail.com>
2025-08-20 15:38:43 +0100

    add updateTaskStatus Function

--- index.php
+++ index.php
@@ -100,6 +100,7 @@ $stats = getTaskStats($pdo);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Task Manager</title>
+    <link rel="stylesheet" href="style.css" />
 </head>
 <body>
     <div class="container">
